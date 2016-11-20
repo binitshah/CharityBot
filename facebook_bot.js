caac337e1a4c974abba4b7c6f5e6ea0ffc0e9955
bshah <bshah2016@gmail.com>
2016-11-20 01:19:45 -0500

    maybe now it works?

--- facebook_bot.js
+++ facebook_bot.js
@@ -23,78 +23,56 @@ controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
 
 controller.hears(['address', 'atlanta'], 'message_received', function(bot, message) {
     bot.reply(message, {
-                attachment: {
-                    'type':'template',
-                    'payload':{
-                         'template_type':'generic',
-                         'elements':[
-                           {
-                             'title':'Atlanta',
-                             'image_url':'https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center=33.749249,-84.387314&zoom=25&markers=33.749249,-84.387314',
-                             'subtitle':'Map of Atlanta.',
+        "attachment": {
+            'type':'template',
+            'payload':{
+                 'template_type':'generic',
+                 'elements':[
+                   {
+                     'title':'Atlanta',
+                     'image_url':'https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center=33.749249,-84.387314&zoom=25&markers=33.749249,-84.387314',
+                     'subtitle':'Map of Atlanta.',
 
-                           }
-                         ]
-                    }
-                }
+                   }
+                 ]
+            }
+        }
     });
 });
 
-controller.hears(['shirt'], 'message_received', function(bot, message) {
+controller.hears(['classic'], 'message_received', function(bot, message) {
     bot.reply(message, {
-            attachment: {
-                'type':'template',
-                'payload':{
-                     'template_type':'generic',
-                     'elements':[
-                       {
-                         'title':'Classic White T-Shirt',
-                         'image_url':'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                         'subtitle':'Soft white cotton t-shirt is back in style',
-                         'buttons':[
-                           {
-                             'type':'web_url',
-                             'url':'https://petersapparel.parseapp.com/view_item?item_id=100',
-                             'title':'View Item'
-                           },
-                           {
-                             'type':'web_url',
-                             'url':'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                             'title':'Buy Item'
-                           },
-                           {
-                             'type':'postback',
-                             'title':'Bookmark Item',
-                             'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM100'
-                           }
-                         ]
-                       },
-                       {
-                         'title':'Classic Grey T-Shirt',
-                         'image_url':'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                         'subtitle':'Soft gray cotton t-shirt is back in style',
-                         'buttons':[
-                           {
-                             'type':'web_url',
-                             'url':'https://petersapparel.parseapp.com/view_item?item_id=101',
-                             'title':'View Item'
-                           },
-                           {
-                             'type':'web_url',
-                             'url':'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                             'title':'Buy Item'
-                           },
-                           {
-                             'type':'postback',
-                             'title':'Bookmark Item',
-                             'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM101'
-                           }
-                         ]
-                       }
-                     ]
-                   }
+        attachment: {
+            'type':'template',
+            'payload':{
+                'template_type':'generic',
+                'elements':[
+                    {
+                        'title':'Classic White T-Shirt',
+                        'image_url':'http://petersapparel.parseapp.com/img/item100-thumb.png',
+                        'subtitle':'Soft white cotton t-shirt is back in style',
+                        'buttons':[
+                            {
+                                'type':'web_url',
+                                'url':'https://petersapparel.parseapp.com/view_item?item_id=100',
+                                'title':'View Item'
+                            },
+                            {
+                                'type':'web_url',
+                                'url':'https://petersapparel.parseapp.com/buy_item?item_id=100',
+                                'title':'Buy Item'
+                            },
+                            {
+                                'type':'postback',
+                                'title':'Bookmark Item',
+                                'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM100'
+                            }
+                        ]
+                    }
+                ]
             }
-        });
+        }
+    });
 });
 
 
